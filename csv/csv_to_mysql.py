b9ca71c9a65e97ea05bf49dd2625772839e5d865
amcdunn <amcdunn@mit.edu>
2016-08-15 15:33:41 -0400

    no error on cursor.execute, but warnings

--- csv/csv_to_mysql.py
+++ csv/csv_to_mysql.py
@@ -5,13 +5,16 @@ import traceback
 
 mydb = MySQLdb.connect(host='sql.mit.edu',
     user='amcdunn',
-    passwd=getpass.getpass("Password for sql.mit.edu:"),
+    passwd='freddy8',
+    #passwd=getpass.getpass("Password for sql.mit.edu:"),
     db='amcdunn+LL_dictionary')
 cursor = mydb.cursor()
 
-csv_data = csv.reader(file(raw_input("Enter path of csv file:"),'rU'),dialect=csv.excel_tab)
+csv_data = csv.reader(file("test.csv",'rU'))
+#csv_data = csv.reader(file(raw_input("Enter path of csv file:"),'rU'),dialect=csv.excel_tab)
 for row in csv_data:
     try:
+        print(len(row))
         cursor.execute('INSERT INTO `TABLE 2`' \
               'VALUES("%s", "%s")', 
               row)
