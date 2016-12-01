dfdd92c5326692294d3ad3c74ae775e1c69689c9
Annie M. Dunn <amcdunn@mit.edu>
2016-12-01 16:34:37 -0500

    End of day 12-1-2016

--- examples/imdb-play/load-example.py
+++ examples/imdb-play/load-example.py
@@ -6,11 +6,28 @@ import traceback
 mydb = MySQLdb.connect(host='sql.mit.edu',
     user='amcdunn',
     passwd=getpass.getpass("Password for sql.mit.edu:"),
-    db='amcdunn+cloudmining')
+    db='amcdunn+imdb_sample')
 cursor = mydb.cursor()
 
-create = "source
-create = create[:-1]+") CHARACTER SET utf8; "
+test_csv_path = "../examples/imdb-play/sql/imdb.csv"
+num_header_rows = 3
+tbl_name = "reload_test"
+
+create = "DROP TABLE IF EXISTS {0}; \
+            CREATE TABLE {0} (".format(tbl_name)
+
+csv_data = csv.reader(file(test_csv_path,'rU'))
+
+header_rows = []
+for counter, row in enumerate(csv_data):
+    header_rows.append(row)
+    if counter==num_header_rows-1:
+        break
+col_data = zip(*header_rows)
+for col in col_data:
+    create += "{} {} COMMENT \'{}\',".format(*col)
+
+create = create[:-1]+") CHARACTER SET ascii; "
 print("Command sent to MySQL: "+create)
 cursor.execute(create)
 cursor.close()
